5fc254e5ac075dd779c37803162b7c2810a0aa85
C:\workspace <wjdruf23123@gmail.com>
2022-09-28 22:00:52 +0900

    프로젝트 생성 및 초기 폴더구조 정의

---
+++ source/kis-manager-server/app.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express' 
+
+const app = express() 
+
+app.get( '', ( req: Request, res: Response ) => { 
+  res.send( 'hello word' ) 
+} ) 
+
+
+app.listen( 3000 )

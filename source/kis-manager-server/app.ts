930fd0c8167d836d3daf559cbd4b5ef50f86a747
C:\workspace <wjdruf23123@gmail.com>
2022-10-08 22:32:00 +0900

    배포 관련 작업 중

--- source/kis-manager-server/app.ts
+++ source/kis-manager-server/app.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response } from 'express' 
+import fs from 'fs'
 
 const app = express() 
 
 app.get( '', ( req: Request, res: Response ) => { 
-  res.send( 'hello word' ) 
+  const html = fs.readFileSync('index.html');
+  res.writeHead(200);
+  res.write(html);
+  res.end();
 } ) 
 
+
+app.get( '/test', ( req: Request, res: Response ) => { 
+  res.send( 'hello word' ) 
+} ) 
 
 app.listen( 3000 )
